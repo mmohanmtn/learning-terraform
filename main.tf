914956ed9a16a007c49fe618b002b3d9e73bd978
mmohanmtn <87228237+mmohanmtn@users.noreply.github.com>
2025-12-14 21:05:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,31 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 data "aws_vpc" "default" {
   default = true
 }
